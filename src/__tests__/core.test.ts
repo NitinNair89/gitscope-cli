2191f74d9068eec50dfe5a20b3ec2a0d82ff16e6
Nitin Chandran Nair <nitin.dreamern89@gmail.com>
2025-07-20 16:12:15 +0530

    feat: accept option format for "markdown" and generate Markdown report

--- src/__tests__/core.test.ts
+++ src/__tests__/core.test.ts
@@ -1,14 +1,21 @@
+import { execSync } from 'child_process';
 import { generateSummary } from '../core/core';
 
+jest.mock('child_process');
+
 describe('core', () => {
-  console.log = jest.fn();
+  const mockExecSync = execSync as jest.Mock;
+
+  beforeEach(() => {
+    mockExecSync.mockReturnValue('===\n1234567890abcdef\nJohn Doe\n2023-10-01 12:00:00 +0000\n');
+  });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should display formatted logs in console', () => {
-    generateSummary(2);
-    expect(console.log).toHaveBeenCalledTimes(3);
+  it('should invoke execSync function to retrieve commit logs', () => {
+    generateSummary(2, 'markdown');
+    expect(mockExecSync).toHaveBeenCalledTimes(1);
   });
 });
--- src/formatters/summary-builder.ts
+++ src/formatters/summary-builder.ts
@@ -1,14 +1,22 @@
-import { ParsedCommitType } from '../types';
+import { OutputFormatType, ParsedCommitType } from '../types';
 import { generateGroupedMarkdown } from './markdown';
 
 /**
  * Generates Markdown and JSON summary from parsed commits.
  * @param commits Array of parsed commits.
- * @returns An object containing markdown and JSON string outputs.
+ * @returns {string} Formatted summary string based on the specified output format.
+ *
+ * @example
+ * generateSummaries(commits, 'json');
  */
-export function generateSummaries(commits: ParsedCommitType[]): { markdown: string; json: string } {
-  return {
-    markdown: generateGroupedMarkdown(commits),
-    json: JSON.stringify(commits, null, 2),
-  };
+export function generateSummaries(
+  commits: ParsedCommitType[],
+  format: OutputFormatType = 'json'
+): string {
+  switch (format) {
+    case 'markdown':
+      return generateGroupedMarkdown(commits);
+    default:
+      return JSON.stringify(commits, null, 2);
+  }
 }
