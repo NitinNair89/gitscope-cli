ba2a99930892e985d3202a245f93e7a800cc6095
Nitin Chandran Nair <nitin.dreamern89@gmail.com>
2025-07-22 01:23:18 +0530

    test: core updated with fs mock

--- src/__tests__/core.test.ts
+++ src/__tests__/core.test.ts
@@ -1,15 +1,20 @@
 import { execSync } from 'child_process';
+import fs from 'fs';
 import { generateSummary } from '../core/core';
 
 jest.mock('child_process');
+jest.mock('fs');
 
 describe('core', () => {
   const mockExecSync = execSync as jest.Mock;
   const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
 
+  const mockReadFileSync = fs.readFileSync as jest.Mock;
+
   beforeEach(() => {
     consoleSpy.mockClear();
     mockExecSync.mockReturnValue('===\n1234567890abcdef\nJohn Doe\n2023-10-01 12:00:00 +0000\n');
+    mockReadFileSync.mockReturnValue(JSON.stringify({ version: '1.0.0' }));
   });
 
   afterEach(() => {
